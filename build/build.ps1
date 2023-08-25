5cafbfcc0065c5cfffb6677346103afd5ed298e9
zephyr <zephyrtmh@gmail.com>
2023-08-25 15:51:07 +0800

    remove build folder from gitignore

---
+++ build/build.ps1
@@ -0,0 +1,12 @@
+# Stop the script if any command fails
+$ErrorActionPreference = "Stop"
+
+Write-Host "build.ps1 running..."
+
+# Run the Maven Wrapper command
+../mvnw clean package -DskipTests -Pexclude-properties
+
+# Copy the application.properties file into the target directory
+Copy-Item -Path "src\main\resources\application.properties" -Destination "config\application.properties"
+
+Write-Host "Build and copy operations completed!"
