e6a10dadf6f5e2c6b606b5b6b2cbcfdbba2bce90
zephyr <zephyrtmh@gmail.com>
2023-08-25 15:55:04 +0800

    change build.sh

--- build/build.ps1
+++ build/build.ps1
@@ -4,7 +4,7 @@ $ErrorActionPreference = "Stop"
 Write-Host "build.ps1 running..."
 
 # Run the Maven Wrapper command
-../mvnw clean package -DskipTests -Pexclude-properties
+./mvnw clean package -DskipTests -Pexclude-properties
 
 # Copy the application.properties file into the target directory
 Copy-Item -Path "src\main\resources\application.properties" -Destination "config\application.properties"
