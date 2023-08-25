aff85e536af2638efaeb5c9c9169b043cf304fc4
zephyr <zephyrtmh@gmail.com>
2023-08-25 16:46:44 +0800

    fix build.ps1

--- build/build.ps1
+++ build/build.ps1
@@ -5,9 +5,10 @@ Write-Host "build.ps1 running..."
 Write-Host $PWD
 Write-Host (Get-ChildItem)
 
+$mvnwPath = "$BUILD_SVR_PATH\$env:CI_COMMIT_REF_NAME\mvn"
+
 # Run the Maven Wrapper command
-Write-Host "$BUILD_SVR_PATH/$env:CI_COMMIT_REF_NAME/mvnw"
-$BUILD_SVR_PATH/$env:CI_COMMIT_REF_NAME/mvnw clean package -DskipTests -Pexclude-properties
+& $mvnwPath clean package -DskipTests -Pexclude-properties
 
 # Copy the application.properties file into the target directory
 Copy-Item -Path "src\main\resources\application.properties" -Destination "config\application.properties"
